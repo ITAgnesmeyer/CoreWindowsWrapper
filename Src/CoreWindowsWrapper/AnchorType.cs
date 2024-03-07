eb9ea638363904a03557862449cf7b06b1dcb97b
ITAgnesmeyer <guido.agnesmeyer@ITAgnesmeyer.com>
2024-03-07 03:51:25 +0100

    Anchor

---
+++ Src/CoreWindowsWrapper/AnchorType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreWindowsWrapper
+{
+    [Flags]
+    public enum AnchorType
+    {
+        Top = 0x01,
+        Bottom = 0x02,
+        Left = 0x04,
+        Right = 0x08,
+        None = 0,
+    }
+}
--- Src/CoreWindowsWrapper/NativeControlBase.cs
+++ Src/CoreWindowsWrapper/NativeControlBase.cs
@@ -5,15 +5,6 @@ using CoreWindowsWrapper.Win32ApiForm;
 
 namespace CoreWindowsWrapper
 {
-    [Flags]
-    public enum AnchorType
-    {
-        Top = 0x01,
-        Bottom = 0x02,
-        Left = 0x04,
-        Right = 0x08,
-        None = 0,
-    }
 
     public class NativeControlBase : IControl
     {
